f1cfc0cc8b7094c76a1bf44bbb00523514edb89f
Mirlanda Costa <mirlanda.dsc@gmail.com>
2024-04-20 23:08:05 -0400

    Subindo automação do menu

--- cypress/e2e/menu.cy.js
+++ cypress/e2e/menu.cy.js
@@ -1,7 +1,34 @@
 describe('Login', () => {
     beforeEach(() => {
         cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+        cy.fillLoginFields()
     })
 
+    it('Valid the Dashboard menu is selected after logging', () => {
+        cy.get('.active')
+            .contains('Dashboard')
+            .should('have.text', 'Dashboard')
+    })
     
+    it('Valid searching by menu', () => {
+        cy.get("input[placeholder='Search']")
+            .type('Leave')
+        
+        cy.get('a span')
+            .should('have.text', 'Leave')
+
+    })
+
+    it('Valid that nothing is returned after searching for an invalid menu', () => {
+        cy.get("input[placeholder='Search']").type('ssshht')
+        cy.get('.oxd-sidepanel-body').should('have.text', '')
+    })
+
+    it('Valid that another menu is opened when it is clicked', () => {
+        cy.contains('Admin').click()
+
+        cy.get('.active')
+            .contains('Admin')
+            .should('have.text', 'Admin')
+    })
 })
